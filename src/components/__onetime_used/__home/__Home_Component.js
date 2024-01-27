8f27bccb21ef4ff53ec7a4d8e4820bca328f6746
Oliulla <razasm29@gmail.com>
2024-01-27 14:42:19 +0600

    [CHANGED]: In home component

--- src/components/__onetime_used/__home/__Home_Component.js
+++ src/components/__onetime_used/__home/__Home_Component.js
@@ -1,18 +1,17 @@
 import FeaturedCategoryCard from '@/components/__shared_multiple_time/__featured_categories/__Featured_Category_Card';
 import FeaturedProductCard from '@/components/__shared_multiple_time/__featured_products/__Featured_Product_Card';
-import React from 'react';
 
 const HomeComponent = () => {
     return (
-        <div>
+        <div className='py-4'>
             <div className='flex flex-col gap-y-10'>
                 <div>
-                    <h1>Featured Products</h1>
-                    <FeaturedProductCard />
+                    <h1 className='text-3xl font-semibold inline border-b-2 border-blue-400'>Featured Products</h1>
+                    <div className='mt-2'><FeaturedProductCard /></div>
                 </div>
                 <div>
-                    <h1>Featured Categories</h1>
-                    <FeaturedCategoryCard />
+                    <h1 className='text-3xl font-semibold inline border-b-2 border-blue-400'>Featured Categories</h1>
+                    <div className='mt-2'><FeaturedCategoryCard /></div>
                 </div>
             </div>
         </div>
