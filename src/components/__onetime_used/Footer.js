ffd40725f0619f77403b84a0095d96553a383ba5
Oliulla <razasm29@gmail.com>
2024-01-27 13:12:33 +0600

    [ADDED]: Github form, nextauth configurations, etc

--- src/components/__onetime_used/Footer.js
+++ src/components/__onetime_used/Footer.js
@@ -26,7 +26,7 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-                        <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; GetPc. Build PC with us as you want {new Date().getFullYear()} </p>
+                        <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; GetPc. Build PC with us as you want in {new Date().getFullYear()} </p>
                         <div className="order-1 md:order-2">
                             <span className="px-2">About us</span>
                             <span className="px-2 border-l">Contact us</span>
